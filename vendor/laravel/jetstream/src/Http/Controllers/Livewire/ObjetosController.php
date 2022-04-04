73d2a484bad6bd84950202fd59d2e00880411d1b
Eversonjrluis <eversonjrluis>
2022-04-03 21:04:00 -0300

    incluir controller livewire

---
+++ vendor/laravel/jetstream/src/Http/Controllers/Livewire/ObjetosController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Laravel\Jetstream\Http\Controllers\Livewire;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use LaravelQRCode\Facades\QRCode;
+
+class ObjetosController extends Controller
+{
+    /**
+     * Show the user profile screen.
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+         return view('cadastros.cadastro');
+
+    }
+
+    public function exibeExtintor()
+    { 
+          return view('cadastros.listas');
+    }
+
+    public function inspecionar($objeto)
+    {
+          return view('cadastros.inspecionar',compact('objeto'));
+    }
+    public function geraQR($objeto)
+    {
+              return  QRCode::url('http://megatech.systemja.com.br/inspecionar/'.$objeto)
+                  ->setSize(8)
+                  ->setMargin(2)
+                  ->png();
+    }
+
+}
